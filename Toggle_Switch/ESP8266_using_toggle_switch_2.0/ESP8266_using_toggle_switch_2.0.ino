5cf50ee6a0ff01887a5e358ca166c5cddd261c8e
ONKAR-SURFACE\onkar <onkargulavani@live.com>
2018-02-19 00:14:07 -0500

    added ".h" file implementaion to make user data common.

--- Toggle_Switch/ESP8266_using_toggle_switch_2.0/ESP8266_using_toggle_switch_2.0.ino
+++ Toggle_Switch/ESP8266_using_toggle_switch_2.0/ESP8266_using_toggle_switch_2.0.ino
@@ -3,30 +3,16 @@
 #include <Servo.h>
 #include <ArduinoOTA.h>
 
-//-------------- String declairation--------------------------
-const char* ssid = "LakeViewWiFi";
-const char* password = "P@ssLakeView";
-const char* mqtt_server = "192.168.2.12";
-const char* mqtt_uname = "onkar20";
-const char* mqtt_pass = "onkar20";
-const char* mqtt_device_name = "ESP8266HallSwitch";
-const char* ota_device_name = "OTA_Hall_Switch";
-const char* ota_password = "onkar20";
+#include "user_data.h"
 
 //-------------variable declaration
-#define servo 4
 long lastMsg = 0;
 char msg[50];
 int value = 0;
-const int lightPin = 2;
 const int posOn = 150;
 const int posOff = 45;
 const int posNormal = 100;
-const int switchPin = 0;
 int switchStatus = 0;
-
-WiFiClient espClient;
-PubSubClient client(espClient);
 
 Servo s1; //servo 1
 
@@ -45,59 +31,7 @@ void setup() {
   digitalWrite(lightPin, LOW);
 }
 
-//----------------------------------------------------------------------------------------------------
-void setup_OTA() {
-  // Port defaults to 8266
-  ArduinoOTA.setPort(8266);
 
-  // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname(ota_device_name);
-
-  // No authentication by default
-  ArduinoOTA.setPassword((const char *)ota_password);
-
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
-}
-//----------------------------------------------------------------------------------------------------
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    digitalWrite(lightPin, !digitalRead(lightPin));
-  }
-
-  digitalWrite(lightPin, HIGH);
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
 //----------------------------------------------------------------------------------------------------
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived new [");
